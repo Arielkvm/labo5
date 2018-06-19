fbccaee57fe99623b4064ca7fb78d63060e0d2bc
kevit <kevit@DESKTOP-695CME9>
2018-06-19 04:14:44 -0600

    labo 5

---
+++ src/labo5/Labo_5.java
@@ -0,0 +1,14 @@
+package labo5;
+
+import gui.Gui;
+
+/**
+ * @author kevin
+ */
+public class Labo_5 {
+
+    public static void main(String[] args) {
+        new Gui().setVisible(true);
+    }
+    
+}
